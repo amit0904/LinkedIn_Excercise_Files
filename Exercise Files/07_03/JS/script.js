abf7ee6ba944886be65308f47f957a76d664053a
Amit <sharma.amit0904@gmail.com>
2019-10-30 22:55:20 +1100

    keep updated

--- Exercise Files/07_03/JS/script.js
+++ Exercise Files/07_03/JS/script.js
@@ -1,1 +1,23 @@
+const CTA = document.querySelector(".cta a");
+console.log(CTA);
+const ALERT = document.querySelector("#booking-alert")
+console.log(ALERT);
+
+CTA.classList.remove("hide");
+ALERT.classList.add("hide");
+
+function reveal(e,current) {
+
+    e.preventDefault();
+    current.innerHTML == "Book Now" ? CTA.innerHTML = "Oooops!" : CTA.innerHTML = "Book Now";
+    //CTA.classList.toggle("hide");
+    ALERT.classList.toggle("hide");
+
+}
+
+//CTA.onclick = reveal;     
+//CTA.onclick = console.log("I am second function for click");
+
+CTA.addEventListener("click", function(e){reveal(e,this);}, false);
+CTA.addEventListener("click", function(){console.log("I am second function for click")}, false);
 
