87846dbf991a96ecb7c19be4d40e1f9bf16ffc67
Amit <sharma.amit0904@gmail.com>
2019-12-07 16:32:32 +1100

    commit message

--- Exercise Files/09_02/JS/script.js
+++ Exercise Files/09_02/JS/script.js
@@ -1,1 +1,12 @@
 
+var extLinks = document.querySelectorAll('a[href^="http"]');
+console.log(extLinks);  
+
+
+for (var i = 0; i < extLinks.length; i++) {
+console.log(extLinks[i]);
+extLinks[i].setAttribute("target","_blank") ;
+if (i ==2 ) {
+    break;
+}
+}
